d45e82de81258690107665794c1fd4005d645119
DESKTOP-4028UN2\greenguitarhero <coolsouro@gmail.com>
2018-12-11 16:10:15 +0530

    Code created for snapping

---
+++ Assets/EditSnap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditSnap : MonoBehaviour
+{
+  
+
+    [SerializeField] [Range(0f, 20f)] float gridsize = 10f;
+    void Update()
+    {
+        Vector3 snappos;
+        snappos.x = Mathf.RoundToInt(transform.position.x / gridsize) * 10f;
+        snappos.z = Mathf.RoundToInt(transform.position.z / gridsize) * 10f;
+
+        transform.position = new Vector3(snappos.x, 0f, snappos.z);
+    }
+}
