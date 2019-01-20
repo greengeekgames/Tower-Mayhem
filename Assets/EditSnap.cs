2f92c5bc8554e749bbfc84513ac79be988b60195
DESKTOP-4028UN2\greenguitarhero <coolsouro@gmail.com>
2019-01-21 01:07:01 +0530

    Grid Complete and enemy is moving around.

--- Assets/EditSnap.cs
+++ Assets/EditSnap.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+
 public class EditSnap : MonoBehaviour
 {
-  
+    Vector3 snappos;
+    Waypoint waypoint;
 
-    [SerializeField] [Range(0f, 20f)] float gridsize = 10f;
-    TextMesh textmesh;
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+
     void Update()
     {
-        Vector3 snappos;
-        snappos.x = Mathf.RoundToInt(transform.position.x / gridsize) * 10f;
-        snappos.z = Mathf.RoundToInt(transform.position.z / gridsize) * 10f;
+        Snaptopos();
+        updatelabel();
+    }
 
-        transform.position = new Vector3(snappos.x, 0f, snappos.z);
-
-        textmesh = GetComponentInChildren<TextMesh>();
-        string label= "(" + snappos.x/10 + "," + snappos.z/10 + ")";
+    private void updatelabel()
+    {
+        int gridsize = waypoint.GetGridSize();
+        TextMesh textmesh = GetComponentInChildren<TextMesh>();
+        string label = "(" + waypoint.GetGridPos().x/ gridsize *10+ "," + waypoint.GetGridPos().y/ gridsize*10 + ")";
         textmesh.text = label;
         gameObject.name = label;
+    }
+
+    private void Snaptopos()
+    {
+
+        int gridsize = waypoint.GetGridSize();
+       // snappos.x = Mathf.RoundToInt(transform.position.x / gridsize) * 10f;
+       // snappos.z = Mathf.RoundToInt(transform.position.z / gridsize) * 10f;
+
+        transform.position = new Vector3(waypoint.GetGridPos().x*10, 0f, waypoint.GetGridPos().y*10);
+        
     }
 }
